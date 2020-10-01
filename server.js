30a97e7bea69159c8508432b740fa0b301c4c330
Felix-Suen <felix.suen2000@gmail.com>
2020-09-30 23:15:42 -0700

    user model and register

--- server.js
+++ server.js
@@ -6,7 +6,16 @@ const app = express();
 //connect database
 connectDB();
 
+//init Middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.send('API Running'));
+
+// define Routes
+app.use('/api/users', require('./routes/api/users.js'));
+app.use('/api/auth', require('./routes/api/auth.js'));
+app.use('/api/profile', require('./routes/api/profile.js'));
+app.use('/api/posts', require('./routes/api/posts.js'));
 
 const PORT = process.env.PORT || 5000;
 
