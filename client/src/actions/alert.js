41345323f5b1c8812b00e0fc18d29039e7305fd4
Felix-Suen <felix.suen2000@gmail.com>
2020-10-20 23:42:31 -0700

    dispatch

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -3,7 +3,6 @@ import uuid from 'uuid';
 
 export const setAlert = (msg, alertType) => dispatch => {
     const id = uuid.v4();
-
     dispatch({
         type: SET_ALERT,
         payload: { msg, alertType, id }
