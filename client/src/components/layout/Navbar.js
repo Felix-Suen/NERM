813b31e957de567c19e54c1cc958bd9d8f173966
Felix-Suen <felix.suen2000@gmail.com>
2020-10-25 23:59:36 -0700

    dashboard fix

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -8,6 +8,12 @@ export const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
     const authLinks = (
         <ul>
             <li>
+                <Link to="/dashboard">
+                <i className="fas fa-user"></i>{' '}
+                <span className="hide-sm"></span>
+                Dashboard</Link>
+            </li>
+            <li>
                 <a onClick={logout} href="#!">
                 <i className="fas fa-sign-out-alt"></i>{' '}
                 <span className="hide-sm">
@@ -29,7 +35,7 @@ export const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
             <h1>
                 <Link to="/">
                     <i className="fas fa-code" /> DevConnector
-        </Link>
+                </Link>
             </h1>
             <Fragment>{isAuthenticated ? authLinks : guestLinks}</Fragment>
         </nav>
