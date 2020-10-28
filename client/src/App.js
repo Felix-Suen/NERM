8ba738fe92e190e8444eb18bcfb99755c7910cfd
Felix-Suen <felix.suen2000@gmail.com>
2020-10-28 16:49:52 -0700

    individual profile link

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,7 @@ import EditProfile from './components/profile-forms/EditProfile';
 import PrivateRoute from './components/routing/PrivateRoute';
 import AddExperience from './components/profile-forms/AddExperience';
 import Profiles from './components/profiles/Profiles';
+import Profile from './components/profile/Profile';
 import AddEducation from './components/profile-forms/AddEducation';
 //Redux
 import { Provider } from 'react-redux';
@@ -40,6 +41,7 @@ const App = () => {
                         <Route exact path="/register" component={ Register }/>
                         <Route exact path="/login" component={ Login }/>
                         <PrivateRoute exact path='/profiles' component={ Profiles } />
+                        <PrivateRoute exact path='/profile/:id' component={ Profile } />
                         <PrivateRoute exact path='/dashboard' component={ Dashboard }/>
                         <PrivateRoute exact path='/create-profile' component={ CreateProfile }/>
                         <PrivateRoute exact path='/edit-profile' component={ EditProfile }/>
