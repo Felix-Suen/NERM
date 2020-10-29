25f40b89fa138a45e92a6dfab57e3e4e23079794
Felix-Suen <felix.suen2000@gmail.com>
2020-10-29 15:13:33 -0700

    post UI

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import { getPosts } from '../../actions/post';
+import PostItem from './PostItem';
 
 const Posts = ({ getPosts, post: { posts, loading } }) => {
     useEffect(() => {
@@ -10,9 +11,20 @@ const Posts = ({ getPosts, post: { posts, loading } }) => {
     }, [getPosts])
     
     return (
-        <div>
-            
-        </div>
+        loading ? <Spinner /> : (
+            <Fragment>
+                <h1 className="large text-primary">Posts</h1>
+                <p className="lead">
+                    <i className="fas fa-user"></i>Welcome to the community
+                </p>
+                {/* Post Form */}
+                <div className="posts">
+                    {posts.map(post => (
+                        <PostItem key={post._id} post={post} />
+                    ))}
+                </div>
+            </Fragment>
+        )
     )
 }
 
