03f82bd2a796db86f4085e1919a557b3384bdf30
Felix-Suen <felix.suen2000@gmail.com>
2020-10-29 15:49:35 -0700

    add like and delete like

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -8,32 +8,32 @@ import PostItem from './PostItem';
 const Posts = ({ getPosts, post: { posts, loading } }) => {
     useEffect(() => {
         getPosts();
-    }, [getPosts])
-    
-    return (
-        loading ? <Spinner /> : (
-            <Fragment>
-                <h1 className="large text-primary">Posts</h1>
-                <p className="lead">
-                    <i className="fas fa-user"></i>Welcome to the community
-                </p>
-                {/* Post Form */}
-                <div className="posts">
-                    {posts.map(post => (
-                        <PostItem key={post._id} post={post} />
-                    ))}
-                </div>
-            </Fragment>
-        )
-    )
-}
+    }, [getPosts]);
+
+    return loading ? (
+        <Spinner />
+    ) : (
+        <Fragment>
+            <h1 className="large text-primary">Posts</h1>
+            <p className="lead">
+                <i className="fas fa-user"></i>Welcome to the community
+            </p>
+            {/* Post Form */}
+            <div className="posts">
+                {posts.map((post) => (
+                    <PostItem key={post._id} post={post} />
+                ))}
+            </div>
+        </Fragment>
+    );
+};
 
 Posts.propTypes = {
     getPosts: PropTypes.func.isRequired,
     post: PropTypes.object.isRequired,
-}
+};
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
     post: state.post,
 });
 
