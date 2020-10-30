f76a1d3928b9a81fdc72b0f65368f446e61a25b1
Felix-Suen <felix.suen2000@gmail.com>
2020-10-30 13:46:11 -0700

    heroku deploy files

---
+++ client/build/precache-manifest.f55a8a53825dbad4f57c2096c5059df9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c168338191fb08849a57c1f9d837bc0c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "14e52a54dc7036df5b36",
+    "url": "/static/css/main.28ec31e2.chunk.css"
+  },
+  {
+    "revision": "3326ca0248ff4785ad13",
+    "url": "/static/js/2.ab2ab27a.chunk.js"
+  },
+  {
+    "revision": "07d28acefe4f478bb7a9400a0790acb9",
+    "url": "/static/js/2.ab2ab27a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14e52a54dc7036df5b36",
+    "url": "/static/js/main.3c0a5ca2.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  }
+]);
